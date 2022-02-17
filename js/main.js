fa0b81d61751dea4d3d79658483edabac466508d
pietroline <pietrobarberi@hotmail.it>
2022-02-17 17:52:03 +0100

    fix url API

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ const vue = new Vue(
 
                if(this.emailArray.length == 0){
                     for(let i=0; i<10; i++){
-                        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                        axios.get('https://flynn.boolean.careers/exercises/api/random/mail') 
                             .then(response => {
                                 this.emailArray.push(response.data.response);
                             })
