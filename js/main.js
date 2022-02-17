b2f60a63fa47a26d2c60bc0a1ad85887680d9871
pietroline <pietrobarberi@hotmail.it>
2022-02-17 16:30:28 +0100

    generare indirizzi email tramite API

---
+++ js/main.js
@@ -0,0 +1,49 @@
+const vue = new Vue(
+
+    {
+        el:"#app",
+        data:{
+            emailArray: [],
+            erroreVerificato: false,
+            view_hide: false,
+            message: "",
+            info: false,
+        },
+
+        methods:{
+
+            createEmailArray(){
+
+               if(this.emailArray.length == 0){
+                    for(let i=0; i<10; i++){
+                        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                            .then(response => {
+                                this.emailArray.push(response.data.response);
+                            })
+                            .catch(errore => this.erroreVerificato = true);
+                    }
+                    this.message = "Hai generato un array di email";
+               }else{
+                   this.message = "Hai già generato un array di email";
+                   this.info = true;
+                   setTimeout(() => {
+                       this.message = "Hai generato un array di email";
+                       this.info = false;
+                   },1000);
+               }
+              
+
+            },
+
+            draws(){
+                this.view_hide = !this.view_hide;
+            }
+
+        }
+        
+    }
+
+);
+
+
+
